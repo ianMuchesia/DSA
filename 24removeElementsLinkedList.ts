80de3b758f8e42e25fc155ec7c1a4e49f0e138d9
ianMuchesia <ianmuchesia@yahoo.com>
2023-11-06 16:19:05 +0300

    removeLinked list

--- 24removeElementsLinkedList.ts
+++ 24removeElementsLinkedList.ts
@@ -1,52 +1,47 @@
 import { LinkedListNode } from "./LinkedList";
 
-
-
-
 const node1 = new LinkedListNode(1);
 const node2 = new LinkedListNode(2);
-const node3 = new LinkedListNode(6);
-const node4 = new LinkedListNode(3);
-const node5 = new LinkedListNode(4);
-const node6 = new LinkedListNode(5);
-const node7 = new LinkedListNode(6);
+const node3 = new LinkedListNode(2);
+const node4 = new LinkedListNode(1);
+// const node5 = new LinkedListNode(4);
+// const node6 = new LinkedListNode(5);
+// const node7 = new LinkedListNode(6);
 
 node1.next = node2;
 node2.next = node3;
 node3.next = node4;
-node4.next = node5;
-node5.next = node6;
-node6.next = node7;
+// node4.next = node5;
+// node5.next = node6;
+// node6.next = node7;
 
+function removeElements(
+  head: LinkedListNode | null,
+  val: number
+): LinkedListNode | null {
+  // console.log(prev)
 
-function removeElements(head: LinkedListNode | null, val: number): LinkedListNode | null {
+  while (head && head.val === val) {
+    head = head.next;
+  }
 
+  let current = head;
+  let prev: LinkedListNode | null = null;
 
-    
-
-    let prev = head;
-    let current = head?.next;
-   
-    let node = prev;
-
-    while(current){
-        
-        if (current.next && current.next.val === val) {
-            console.log(current.next.val)
-            if (prev) prev.next = current.next.next;
-            current = current.next
-          }
-
-          if(current)current = current.next
-          if (prev) prev = prev.next;
-    
-        
+  while (current) {
+    if (current.val === val) {
+      if (prev) {
+        prev.next = current.next;
+      }
+    } else {
+      prev = current;
     }
+    current = current.next;
+  }
+ 
 
-    console.log(prev)
+  console.log(prev)
+  return head;
+}
 
-    return node
-    
-};
-
-console.log(removeElements(node1,6))
+console.log(removeElements(node1, 2));
